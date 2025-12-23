38339de43d46a6a07a917ff7c31bb786a5606ea8
he1enaluik <helenakrislin.luik@tptlive.ee>
2025-12-23 12:56:23 +0200

    dasdad
    asdd

---
+++ app/pages/login.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import axios from 'axios';
+    axios.defaults.withXSRFToken = true;
+    axios.defaults.withCredentials = true;
+    let res = await axios.get('http://localhost:8000/sanctum/csrf-cookie');
+    console.log(res);
+    res = await axios.post('http://localhost:8000/api/login', {
+        email: 'kasparsu@gmail.com',
+        password: 'password',
+    });
+    res = await axios.get('http://localhost:8000/api/user');
+    console.log(res);
+</script>
+<template>
+
+</template>
